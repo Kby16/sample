fea1bb2ea5b87dac44d0f8fe51cfbe1b9da0f050
Kby16 <kbykrishnabalaji@gmail.com>
2025-03-24 08:56:42 -0700

    2nd update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and git
+          Learn React and git and push and pull
         </a>
       </header>
     </div>
